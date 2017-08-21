fd3997a4bb2dfb44b76bb2935e52e3b0902e2ed0
Michael Schenk <michael.schenk@weather.com>
2017-08-20 20:07:53 -0500

    Property tests

--- property.go
+++ property.go
@@ -2,7 +2,6 @@ package gopack
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 )
@@ -16,32 +15,6 @@ func (p *Properties) Merge(props *Properties) {
 	for k, v := range *props {
 		(*p)[k] = v
 	}
-}
-
-func (p *Properties) Bool() bool {
-	return false
-}
-
-func (p *Properties) Float() float64 {
-	return 0
-}
-
-func (p *Properties) Str(key string) (string, bool) {
-	v, f := (*p)[key]
-	switch v.(type) {
-	case string:
-		return v.(string), f
-	default:
-		panic(fmt.Sprintf("unable to convert %s to string", key))
-	}
-}
-
-func (p *Properties) StrSlice() []string {
-	return []string{}
-}
-
-func (p *Properties) StrMap() map[string]string {
-	return map[string]string{}
 }
 
 func (p *Properties) Load(filename string) error {
--- property_test.go
+++ property_test.go
@@ -1,6 +1,7 @@
 package gopack
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +15,92 @@ func TestMarshalJSON(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal("/var/log/nginx", p["nginx.log_dir"])
 	assert.Equal("/var/cache", p["nginx.cache_dir"])
+}
+
+func TestMerge(t *testing.T) {
+	assert := assert.New(t)
+
+	b := []byte(`{"nginx.log_dir":"/var/log/nginx", "nginx.cache_dir": "/var/cache"}`)
+	p := Properties{}
+	err := p.unmarshalJSON(b)
+
+	b = []byte(`{"nginx.cache_dir": "/etc/cache"}`)
+	override := &Properties{}
+	err = p.unmarshalJSON(b)
+	assert.NoError(err)
+
+	assert.NotPanics(func() { p.Merge(override) })
+	assert.Equal(Properties{"nginx.log_dir": "/var/log/nginx", "nginx.cache_dir": "/etc/cache"}, p)
+}
+
+func TestStringType(t *testing.T) {
+	assert := assert.New(t)
+	b := []byte(`{"string": "val"}`)
+	p := Properties{}
+	err := p.unmarshalJSON(b)
+	assert.NoError(err)
+	assert.Equal("val", p["string"].(string))
+}
+
+func TestFloat64Type(t *testing.T) {
+	assert := assert.New(t)
+	b := []byte(`{"float":0.1}`)
+	p := Properties{}
+	err := p.unmarshalJSON(b)
+	assert.NoError(err)
+	assert.Equal(0.1, p["float"].(float64))
+}
+
+func TestIntType(t *testing.T) {
+	assert := assert.New(t)
+	b := []byte(`{"int": 1}`)
+	p := Properties{}
+	err := p.unmarshalJSON(b)
+	assert.NoError(err)
+	assert.Equal(1, int(p["int"].(float64)))
+}
+
+func TestMapType(t *testing.T) {
+	assert := assert.New(t)
+	b := []byte(`{"map": {"key1": "val1", "key2": "val2"}}`)
+	p := Properties{}
+	err := p.unmarshalJSON(b)
+	assert.NoError(err)
+	assert.Equal(map[string]interface{}{"key1": "val1", "key2": "val2"}, p["map"].(map[string]interface{}))
+}
+
+func TestArrayType(t *testing.T) {
+	assert := assert.New(t)
+	b := []byte(`{"array": ["val1", "val2"]}`)
+	p := Properties{}
+	err := p.unmarshalJSON(b)
+	assert.NoError(err)
+	assert.Equal([]interface{}{"val1", "val2"}, p["array"].([]interface{}))
+}
+
+func TestBoolType(t *testing.T) {
+	assert := assert.New(t)
+	b := []byte(`{"bool": true}`)
+	p := Properties{}
+	err := p.unmarshalJSON(b)
+	assert.NoError(err)
+	assert.Equal(true, p["bool"].(bool))
+}
+
+func TestNilType(t *testing.T) {
+	assert := assert.New(t)
+	b := []byte(`{"nil": null }`)
+	p := Properties{}
+	err := p.unmarshalJSON(b)
+	assert.NoError(err)
+	assert.Equal(nil, p["nil"])
+}
+
+func TestWrongType(t *testing.T) {
+	assert := assert.New(t)
+	b := []byte(`{"string": "val"}`)
+	p := Properties{}
+	err := p.unmarshalJSON(b)
+	assert.NoError(err)
+	assert.Panics(func() { fmt.Println("invalid type assertion, shouldn't print", p["string"].(float64)) })
 }
